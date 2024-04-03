6d253fc027b839384fd4d2f6304b0c39619ec297
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-04-03 13:04:18 +0400

    js start

---
+++ JavaScript/lesson1-intoduction/js/script.js
@@ -0,0 +1,15 @@
+// one line 
+/**
+ * 
+ * more 
+ * line
+ */
+
+// debugger;
+/*
+console.log("hello js 1");
+console.log("hello js 2");
+console.log("hello js 3");
+console.log("hello js 4");
+console.log("hello js 5");
+*/
---
+++ JavaScript/lesson2-datatypes/js/script.js
@@ -0,0 +1,33 @@
+// Primitive DT
+// string
+// var a = "Rust";
+// console.log(a+" is programming language!");
+// number
+// var info = 5;
+// console.log(info+10);
+// boolean
+// true | false
+// var check = false;
+// if (check) {
+//     console.log('this is true');
+// }else{
+//     console.log('this is false');
+// }
+
+// Composite DT
+// Array
+// var info = [5,10,"hello",true];
+// console.log(info);
+// Object
+// var info = {
+//     name:"Cumshud",
+//     age:23
+// }
+// console.log(info.age);
+
+// Special DT
+// var a;
+// console.log(a);
+
+// var a = null;
+// console.log(a);
