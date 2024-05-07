e17d6fe919762535a63d283b8438eee4933e9418
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-05-07 15:03:11 +0400

    dom part2

--- JavaScript/lesson12-DOM-part2/js/script.js
+++ JavaScript/lesson12-DOM-part2/js/script.js
@@ -9,18 +9,56 @@
 
 
 
-const btn = document.querySelector('button');
-const menu = document.querySelector('menu');
-btn.onclick = ()=>{
-  if (!menu.className ) {
-    document.querySelector('menu').classList.add("open");
-  }else{
-    document.querySelector('menu').classList.remove("open");
-  }
-}
+// const btn = document.querySelector('button');
+// const menu = document.querySelector('menu');
+// btn.onclick = ()=>{
+//   if (!menu.className ) {
+//     document.querySelector('menu').classList.add("open");
+//   }else{
+//     document.querySelector('menu').classList.remove("open");
+//   }
+// }
 // const text  = document.querySelector('h1');
 // const changed = ()=>{
 //     text.className = 'info'
 // }
 // btn.onclick = changed;
 
+
+// const titleBtn = document.querySelector('.title');
+// const accardion = document.querySelector('.accardion');
+// titleBtn.onclick = ()=>{
+//   if (accardion.style.height === "50px") {
+//     accardion.style.height = "350px";
+//   }else{
+//     accardion.style.height = "50px";
+//   }
+// }
+
+
+// document.querySelector('button').onclick=()=>{
+//   document.querySelector('h1').classList.toggle('red')
+// }
+
+
+
+const btn = document.querySelector('#modeBtn');
+const nav = document.querySelector('nav');
+
+const mode = ()=>{
+        if (btn.innerHTML === "Dark") {
+          btn.innerHTML = "Light";
+          nav.className = "navbar navbar-expand-lg bg-dark navbar-dark"
+        }else{
+          nav.className = "navbar navbar-expand-lg bg-light navbar-light"
+          
+          btn.innerHTML = "Dark";
+        }
+}
+
+btn.onclick = mode;
+
+const a  = [{a:"a"}];
+
+
+console.log(...a);
