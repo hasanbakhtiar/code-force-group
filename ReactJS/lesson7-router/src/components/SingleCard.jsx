87169c872e3662e37adeef06e424ed9244bdf1ff
hasanbakhtiar <hasan.doctype@gmail.com>
2024-06-29 13:32:31 +0400

    router

--- ReactJS/lesson7-router/src/components/SingleCard.jsx
+++ ReactJS/lesson7-router/src/components/SingleCard.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { Link } from "react-router-dom";
+import slugify from "slugify";
 
 const SingleCard = ({alldata}) => {
     const [textLength,setTextLength] = useState(20);
@@ -9,7 +11,7 @@ const SingleCard = ({alldata}) => {
                 <div className="card-body">
                     <h5 className="card-title">{alldata.title}</h5>
                     <p className="card-text">{alldata.description.slice(0,textLength)}<span onClick={()=>{setTextLength(alldata.description.length)}}>{textLength===alldata.description.length?"":"..."}</span></p>
-                    <a href="#" className="btn btn-dark">Read more</a>
+                    <Link to={`/products/${slugify(alldata.title)}`} className="btn btn-dark">Read more</Link>
                 </div>
             </div>
         </div>
---
+++ ReactJS/lesson7-router/src/pages/ProductDetails.jsx
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import slugify from 'slugify';
+
+const ProductDetails = () => {
+    const {slug} = useParams();
+    const [productData,setProductData] = useState([]);
+    useEffect(()=>{
+        axios.get('https://dummyjson.com/products')
+        .then(res=>setProductData(res.data.products))
+    },[])
+    const productDetails = productData.find(p=>slugify(p.title) == slug)
+
+  return (
+    <>
+    {!productDetails?<div className='d-flex align-items-center justify-content-center'>
+        <img src="https://cdn.dribbble.com/users/2973561/screenshots/5757826/loading__.gif" alt="" />
+    </div>:
+<div className="container col-xxl-8 px-4 py-5">
+  <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+    <div className="col-10 col-sm-8 col-lg-6">
+      <img src={productDetails.images[0]} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width={700} height={500} loading="lazy" />
+    </div>
+    <div className="col-lg-6">
+      <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">{productDetails.title}</h1>
+      <p className="lead">{productDetails.description}</p>
+      <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+        <Link to="/products" type="button" className="mt-5 btn btn-dark btn-lg px-4 me-md-2">Back</Link>
+      </div>
+  </div>
+</div>
+
+
+        </div>}
+    </>
+  )
+}
+
+export default ProductDetails
