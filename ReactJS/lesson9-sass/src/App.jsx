588a98a895b4fd17ae1b3b4edbf453a5851950de
hasanbakhtiar <hasan.doctype@gmail.com>
2024-07-03 12:09:37 +0400

    sass

---
+++ ReactJS/lesson9-sass/src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>Table</li>
+        <li>Pen</li>
+        <li>Chair</li>
+        <li>Book</li>
+        <li>CopyBook</li>
+        <li>CopyBook</li>
+        <li>CopyBook</li>
+        <li>CopyBook</li>
+        <li>CopyBook</li>
+        <li>CopyBook</li>
+        <li>CopyBook</li>
+        <li>CopyBook</li>
+        <li>CopyBook</li>
+        <li>CopyBook</li>
+        <li>CopyBook</li>
+      </ul>
+{/*       
+
+
+<div className='main-box'>
+  <div className="box">
+    <div className="mini-box"></div>
+    <div className="mini-box"></div>
+    <div className="mini-box"></div>
+    <div className="mini-box"></div>
+  </div>
+  <div className="box">  <div className="mini-box"></div>  <div className="mini-box"></div>  <div className="mini-box"></div>  <div className="mini-box"></div></div>
+  <div className="box">    <div className="mini-box"></div>    <div className="mini-box"></div>    <div className="mini-box"></div>    <div className="mini-box"></div></div>
+  <div className="box">    <div className="mini-box"></div>    <div className="mini-box"></div>    <div className="mini-box"></div>    <div className="mini-box"></div></div>
+  <div className="box">    <div className="mini-box"></div>    <div className="mini-box"></div>    <div className="mini-box"></div>    <div className="mini-box"></div></div>
+</div>
+
+       */}
+    </div>
+  )
+}
+
+export default App
